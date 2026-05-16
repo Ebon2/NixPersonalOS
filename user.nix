f9407c8787b958649d836d1a8ebee1619072423d
Angel <luis.mgcn@gmail.com>
2026-05-16 13:18:35 -0600

    configuracion completa para reproductibilidad constante en github

--- user.nix
+++ user.nix
@@ -1,2 +1,10 @@
 {
+  username         = "UserName";
+  fullName         = "Full Name";
+  email            = "YourEmail@mail.com";
+  description      = "Description";
+  
+  # Generate with: mkpasswd -m sha-512
+  hashedPassword   = "HashedPW";
+  gitDefaultBranch = "main";
 }
